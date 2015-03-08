35e37ee28aa049ddbbcc5a9fe41fa931263f816c
Jithesh <mail@jithe.sh>
2015-03-08 14:30:32 +0530

    Adding plot2.png & plot2.R

---
+++ plot2.R
@@ -0,0 +1,18 @@
+## R script to create 'plot2.png'
+
+## Download the zip file for 'Individual household electric power consumption Data Set' made available
+## on the course website and extract it into the working directory
+
+
+## na.strings argument in the read.csv replaces any '?' values with NA
+hpc <- read.csv(file = 'household_power_consumption.txt', sep = ';', head = TRUE, na.strings = '?')
+splitted <- subset(hpc, as.Date(hpc$Date, format = '%d/%m/%Y') == as.Date('2007-02-01', format = '%Y-%m-%d') | as.Date(hpc$Date, format = '%d/%m/%Y') == as.Date('2007-02-02', format = '%Y-%m-%d'))
+
+## Combining Date and Time fields and converting with strptime
+date = as.character(paste(splitted$Date, splitted$Time))
+datetime = strptime(date, format = '%d/%m/%Y %H:%M:%S')
+
+## Creating the plot
+png(file = 'plot2.png', width = 480, height = 480)
+plot(x = datetime, y = splitted$Global_active_power, type ='l', xlab = '', ylab = 'Global Active Power (kilowatts)')
+dev.off()
