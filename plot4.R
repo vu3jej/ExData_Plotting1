a0c54f7ee60cc0f0547aee09a3ee6c2f4aace1a7
Jithesh <mail@jithe.sh>
2015-03-08 17:02:07 +0530

    Replacing the pch argument in the legend with lty

--- plot4.R
+++ plot4.R
@@ -22,7 +22,7 @@ plot(x = datetime, y = splitted$Global_active_power, type ='l', xlab = '', ylab 
 plot(x = datetime, y = splitted$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type ='l')
 points(x = datetime, y = splitted$Sub_metering_2, col = 'red', type = 'l')
 points(x = datetime, y = splitted$Sub_metering_3, col = 'blue', type = 'l')
-legend('topright', legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col = c('black', 'red', 'blue'), pch = '—', bty = 'n')
+legend('topright', legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col = c('black', 'red', 'blue'), lty = 1, bty = 'n')
 
 ## Adding third plot
 plot(x = datetime, y = splitted$Voltage, ylab = 'Voltage', type ='l')
